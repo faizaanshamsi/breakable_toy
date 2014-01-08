a55b282da7719f4191de3f9ab28cbabac63f63e5
faizaanshamsi <faizaanshamsi@gmail.com>
2014-01-08 18:59:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BreakableToy::Application.config.secret_key_base = 'f561a50e1689b02a4e24ba3228d52b4e10992fc25dc45d64ae1005e3aa462036dd97fed5aebec7c5a5147a388318b1dbed0c39e7a15ab4ed65b134a05ac77d77'
