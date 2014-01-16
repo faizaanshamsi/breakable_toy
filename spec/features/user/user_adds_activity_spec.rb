2a4c8041ac7c03b6a3e0a98494ea561bc8f8e76a
faizaanshamsi <faizaanshamsi@gmail.com>
2014-01-16 10:32:28 -0500

    add sample data csv

---
+++ spec/features/user/user_adds_activity_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+feature "User signs up", %q{
+  As an Authenticated User
+  I can choose an activity
+  So that I can earn accomplishments for that activity
+} do
+
+  # Acceptance Criteria:
+  # User will choose activity and set goals
+  # Goals will be time based
+  # Goals will be frequency based
+
+  scenario ''
+
+end
+
+
+
