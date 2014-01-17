2b394f41a5932d49f6e81a4401a162ceb5df85d0
faizaanshamsi <faizaanshamsi@gmail.com>
2014-01-17 11:25:58 -0500

    add spec

---
+++ spec/features/accomplishment/user_adds_accomplishment_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+feature "User adds accomplishment", %q{
+  As an Authenticated User
+  I can choose an activity
+  So that I can earn accomplishments for that activity
+} do
+
+  # Acceptance Criteria:
+  # User may log a chosen activity
+  # User must enter duration
+  # Team and User will earn points
+
+  scenario 'User creates accomplishment' do
+    activity = FactoryGirl.create(:activity)
+    user = FactoryGirl.create(:user)
+    user_activity = FactoryGirl.create(:user_activity, user_id: user.id, activity_id: activity.id)
+    sign_in_as(user)
+    visit user_path(user)
+
+    click_on 'Add Accomplishment'
+    fill_in 'Duration', with: 30
+    click_on 'Submit'
+
+    expect(page).to have_content "#{activity.name}"
+    expect(user.reload.points.count).to eql(1)
+    expect(user.reload.accomplishments.count).to eql(1)
+  end
+end
